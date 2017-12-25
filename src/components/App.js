1da75ebb56f85dfbbf3a49f5a684281a53294c05
Vikram Patil <vikram.vvp@gmail.com>
2017-12-25 20:35:42 +0530

    store architecting in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,42 @@
-import React, { Component } from 'react'
-import { addRecipe } from '../actions'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from '../actions/index';
 
 class App extends Component {
-  state = {
-    calendar: null
-  }
-  componentDidMount () {
-    const { store } = this.props
-
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  }
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      },
-    }))
-
-    this.input.value = ''
+  doThing = () => {
+    this.props.selectRecipe({})
   }
   render() {
+    console.log('props ', this.props);
     return (
       <div>
-        <input
-          type='text'
-          ref={(input) => this.input = input}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+        Hello World!
       </div>
     )
   }
 }
-export default App
+
+function mapStateToProps({food, calendar}) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = 
+          calendar[day][meal] ? 
+          food[calendar[day][meal]]: 
+          null;
+        return meals;
+      }, {})
+    }))
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
